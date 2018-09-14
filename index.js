df5bb7113e1f0e2adc25b15e0e7a542bcb71a578
Andrew <andrewchoi1@hotmail.com>
2018-09-14 10:42:19 -0400

    added jsx

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const h = React.createElement;
-
 let generateId = () => {
-   return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString();
-}
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER).toString();
+};
 const posts = [
     { id: 1, title: '1st comment' },
     { id: 2, title: '2nd comment' },
@@ -14,39 +12,46 @@ class WassupForm extends React.Component {
         super(props);
         this.state = {
             newPost: ''
-        }
+        };
     }
-    
+
     render() {
-        return h('form', {
-        onSubmit: (event) => {
-            event.preventDefault()
-            this.props.addPost(this.state.newPost)
-        }
-    }, 
-        h('input', {type: 'text',
-        value: this.state.newPost,
-        onChange: (event) => {
-            let value = event.target.value;
-            console.log(value)
-            this.setState({ newPost: value })
-        }}), 
-        h('button', {}, 'post')
-    )
+        return <form
+            onSubmit={event => {
+                event.preventDefault();
+                this.props.addPost(this.state.newPost);
+            }}
+        >
+            <input
+                type="text"
+                value={this.state.newPost}
+                onChange={event => {
+                    let value = event.target.value;
+                    this.setState({ newPost: value });
+                }}
+            />
+            <input type="submit" value="Post" />
+        </form>;
     }
 }
 
-let WassupList = (props) => {
-    return h('div', {}, 
-        h('h3', {}, props.posts.map(post => {
-        return h(WassupRow, {post: post, key: post.id} );
-    }))
+let WassupList = props => {
+    return (
+        <div>
+            <h3>
+                {props.posts.map(post => {
+                    return <WassupRow post={post} key={post.id} />;
+                })}
+            </h3>
+        </div>
     );
 };
 
-let WassupRow = (props) => {
-    return h('ul', {}, 
-        h('li', {}, props.post.title)
+let WassupRow = props => {
+    return (
+        <ul>
+            <li>{props.post.title}</li>
+        </ul>
     );
 };
 
@@ -58,7 +63,7 @@ class Homepage extends React.Component {
         };
     }
     render() {
-        let addPost = (newPost) => {
+        let addPost = newPost => {
             this.setState({
                 posts: this.state.posts.concat([
                     {
@@ -66,16 +71,16 @@ class Homepage extends React.Component {
                         title: newPost
                     }
                 ])
-            })
-        }
-        
-        return h('div', {}, 
-            h('h1', {}, 'WassUp!'),
-            h(WassupForm, {addPost: addPost}),
-            h(WassupList, { posts: this.state.posts }),
-            h('h4', {}, 'copyright 2018')
-        );
+            });
+        };
+
+        return <div>
+            <h1>WassUp!</h1>
+            <WassupForm addPost={addPost}/>
+            <WassupList posts={this.state.posts}/>
+            <h4>Copyright 2018</h4>
+        </div>
     }
 }
 
-ReactDOM.render(h(Homepage), document.querySelector('.root'));
+ReactDOM.render(<Homepage/>, document.querySelector('.root'));
